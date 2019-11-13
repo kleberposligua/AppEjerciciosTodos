89ba5c7c59bacc43f283b3accd79f33389ca86d6
kleberposligua <kleberposligua@hotmail.com>
2019-11-13 12:13:16 -0500

    Ejercicio 2.13 area triángulo

---
+++ AppEjerciciosTodos/frmAreaTriangulo.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace AppEjerciciosTodos
+{
+    partial class frmAreaTriangulo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.txtAreaTriangulo = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(320, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "2.13 AREA DEL TRIANGULO CONOCIENDO LOS TRES LADOS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingrese el valor de A";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ingrese el valor de B";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Ingrese el valor de C";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 181);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Area del triángulo";
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(125, 74);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(100, 20);
+            this.txtA.TabIndex = 5;
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(125, 105);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(100, 20);
+            this.txtB.TabIndex = 6;
+            // 
+            // txtC
+            // 
+            this.txtC.Location = new System.Drawing.Point(125, 137);
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(100, 20);
+            this.txtC.TabIndex = 7;
+            // 
+            // txtAreaTriangulo
+            // 
+            this.txtAreaTriangulo.Location = new System.Drawing.Point(125, 174);
+            this.txtAreaTriangulo.Name = "txtAreaTriangulo";
+            this.txtAreaTriangulo.Size = new System.Drawing.Size(100, 20);
+            this.txtAreaTriangulo.TabIndex = 8;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(35, 222);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 9;
+            this.btnCalcular.Text = "&Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(134, 222);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 10;
+            this.btnSalir.Text = "&Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // frmAreaTriangulo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(311, 260);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtAreaTriangulo);
+            this.Controls.Add(this.txtC);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmAreaTriangulo";
+            this.Text = "Area del triángulo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.TextBox txtAreaTriangulo;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
---
+++ AppEjerciciosTodos/frmAreaTriangulo.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppEjerciciosTodos
+{
+    public partial class frmAreaTriangulo : Form
+    {
+        public frmAreaTriangulo()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if (this.txtA.Text.Length == 0)
+            {
+                MessageBox.Show("Debe ingresar el valor de A");
+                this.txtA.Focus();
+                return;
+            }
+            if (this.txtB.Text.Length == 0)
+            {
+                MessageBox.Show("Debe ingresar el valor de B");
+                this.txtB.Focus();
+                return;
+            }
+            if (this.txtC.Text.Length == 0)
+            {
+                MessageBox.Show("Debe ingresar el valor de C");
+                this.txtC.Focus();
+                return;
+            }
+
+            //double a = Double.Parse( this.txtA.Text);
+            double a,b,c,result;
+            if (Double.TryParse(this.txtA.Text, out result))
+                a = result;
+            else 
+            { 
+                MessageBox.Show("Debe ingresar valores numéricos");
+                this.txtA.Focus();
+                return;
+            }
+
+            
+            if (Double.TryParse(this.txtB.Text, out result))
+                b = result;
+            else
+            {
+                MessageBox.Show("Debe ingresar valores numéricos");
+                this.txtB.Focus();
+                return;
+            }
+
+            if (Double.TryParse(this.txtC.Text, out result))
+                c = result;
+            else
+            {
+                MessageBox.Show("Debe ingresar valores numéricos");
+                this.txtC.Focus();
+                return;
+            }
+
+            this.txtAreaTriangulo.Text = fnAreaTriangulo(a, b, c).ToString("0.00001");
+
+        }
+
+        private double fnAreaTriangulo(double a, double b, double c)
+        {
+            double area,sp;
+            sp = (a + b + c) / 2;
+
+            area = Math.Sqrt(sp * (sp - a) * (sp - b) * (sp - c));
+            return area;
+        }        
+    }
+}
